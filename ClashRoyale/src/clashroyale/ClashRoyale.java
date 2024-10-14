6ef37b957e975fa1f9fe4d147b26bed31df3e2a5
RachidChaibugr <rachidchaib@correo.ugr.es>
2024-10-14 20:04:12 +0200

    Test de constructores de Tropa. Closes #6

--- ClashRoyale/src/clashroyale/ClashRoyale.java
+++ ClashRoyale/src/clashroyale/ClashRoyale.java
@@ -14,7 +14,14 @@ public class ClashRoyale {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Tropa t1 = new Tropa();
+        Tropa t2 = new Tropa(2,2,2,2,2);
+        Tropa t3 = new Tropa(t2);
+        
+        System.out.println(t1);
+        System.out.println(t2);
+        System.out.println(t3);
+        
     }
     
 }
