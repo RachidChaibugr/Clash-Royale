7965e42fe9bfa27d0db32abfafa9eb2066598db7
RachidChaibugr <rachidchaib@correo.ugr.es>
2024-10-14 19:53:33 +0200

    Creacion atributos clase e instancia de Tropa. Closes #2

--- ClashRoyale/src/clashroyale/Tropa.java
+++ ClashRoyale/src/clashroyale/Tropa.java
@@ -9,5 +9,16 @@ package clashroyale;
  * @author Rachi
  */
 public class Tropa {
+    public static final int PUNTOS_SALUD = 1;
+    public static final int DANIO = 1;
+    public static final int VELOCIDAD_MOVIMIENTO = 1;
+    public static final int RANGO_ATAQUE = 1;
+    public static final int VELOCIDAD_ATAQUE = 1;
+    
+    private int puntosSalud;
+    private int danio;
+    private int velocidadMovimiento;
+    private int rangoAtaque;
+    private int velocidadAtaque;
     
 }
